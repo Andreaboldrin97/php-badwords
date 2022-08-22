70b6630c854d962d5397c43135e3d2f55c323e48
Andreaboldrin97 <boldrinandrea23@gmail.com>
2022-08-22 15:12:15 +0200

    milestone 3 censorship of the word

--- index.php
+++ index.php
@@ -9,20 +9,26 @@
 </head>
 
 <body>
-    <h1>PHP Project</h1>
-
+    <h1>PHP Project 1</h1>
     <p>
         <?php
         $paragraph = "Luigi Micco ha perso l'areo";
         echo $paragraph . '; Questa frase è lunga : ' . strlen($paragraph);
         ?>
     </p>
-    <h4>Vedi cosa può esclamare Luigi aggiungi una parola .....</h4>
 
+    <h4>Vedi cosa può esclamare Luigi aggiungi una parola .....</h4>
     <p>
         <?php
         $new_paragraph = $paragraph . ' : ' . $_GET['word'];
         echo $new_paragraph . ' ; Questa frase è lunga : ' . strlen($new_paragraph)
+        ?>
+    </p>
+
+    <h4>No.. Luigi queste parole non si dicono , dovrò censurarle prima di metterle nel DOM...</h4>
+    <p>
+        <?php
+        echo str_replace($_GET['word'], '****', $new_paragraph)
         ?>
     </p>
 </body>
